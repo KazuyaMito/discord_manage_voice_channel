8c4ce204514bf73297ae47a15df947b5ca37f644
KazuyaMito <mito@wonderpla.net>
2021-03-24 12:39:34 +0900

    add command link register

--- app/entry.py
+++ app/entry.py
@@ -28,14 +28,14 @@ async def on_voice_state_update(member, before, after):
             print('delete channel: {}'.format(before.channel.id))
 
     if after.channel is not None and after.channel.id in channels.keys():
-        type = channels[after.channel.id]
+        type_name = channels[after.channel.id]
 
-        limit = channel_types[type]
-        channel_name = "{0}_{1}".format(type.upper(), datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+        limit = channel_types[type_name]
+        channel_name = "{0}_{1}".format(type_name.upper(), datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
         category = after.channel.category
         tmp_channel = await category.create_voice_channel(channel_name, user_limit=limit)
         print("create channel: {}".format(tmp_channel.id))
-        tmp_channels[tmp_channel.id] = type
+        tmp_channels[tmp_channel.id] = type_name
         await member.move_to(tmp_channel)
 
 
@@ -61,6 +61,49 @@ async def create(ctx, type_name=None, category_name=None):
         channels[channel.id] = type_name
         await ctx.message.delete()
         print("create management channel: {}".format(channel.id))
+
+
+@bot.command()
+async def link(ctx, channel_name=None, type_name=None):
+    global channels
+
+    if channel_name is None or type_name is None:
+        await ctx.send("チャンネル名とチャンネルタイプは両方入力してください\nex: /link [channel_name] [channel_type]")
+        return
+
+    guild = ctx.guild
+    channel = next(filter(lambda c: c.name == channel_name, guild.voice_channels), None)
+    if channel is None:
+        await ctx.send("チャンネルが見つかりません: {}".format(channel_name))
+        return
+
+    type = next(filter(lambda t: t == type_name, channel_types.keys()), None)
+    if type is None:
+        await ctx.send("チャンネルタイプが見つかりません: {}".format(type_name))
+        return
+
+    channels[channel.id] = type
+    await ctx.send("{}を管理用チャンネルとして登録しました".format(channel.name))
+    print("register management channel: {}".format(channel.id))
+
+
+@bot.command()
+async def register(ctx, type_name=None, limit=0):
+    global channel_types
+
+    if type_name is None:
+        await ctx.send("チャンネルタイプを入力してください\nex: /register [channel_type] [limit(defaul 0)]")
+        return
+
+    if limit < 0:
+        await ctx.send("人数制限を0人未満にすることはできません")
+        return
+
+    channel_types[type_name] = limit
+    limit_message = "ありません" if limit <= 0 else "{}人です".format(limit)
+
+    await ctx.send("{0}を新しいチャンネルタイプとして登録しました\n人数制限は{1}".format(type_name, limit_message))
+    print("register channel type: {0} (limit = {1})".format(type_name, limit))
 
 
 bot.run(settings.TOKEN)
